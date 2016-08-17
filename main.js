3b089761d7105581b4d0f15ed51b76640af5f6b9
aSwift14 <aswift@iinet.net.au>
2016-08-17 20:53:57 +0930

    Added Player!
    Created a new project that contains a player and a keyboard object,,
    looking forward to the next part!

---
+++ main.js
@@ -0,0 +1,85 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+var startFrameMillis = Date.now();
+var endFrameMillis = Date.now();
+
+function getDeltaTime() {
+	endFrameMillis = startFrameMillis;
+	startFrameMillis = Date.now();
+
+	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+
+	if (deltaTime > 1)
+		deltaTime = 1;
+
+	return deltaTime;
+}
+
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
+
+var fps = 0;
+var fpsCount = 0;
+var fpsTime = 0;
+
+
+var chuckNorris = document.createElement("img");
+
+chuckNorris.src = "hero.png";
+
+var player = new Player();
+var keyboard = new Keyboard();
+
+
+function run() {
+	context.fillStyle = "#ccc";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	var deltaTime = getDeltaTime();
+
+	player.update(deltaTime);
+	player.draw();
+
+
+	fpsTime += deltaTime;
+	fpsCount++;
+	if (fpsTime >= 1) {
+		fpsTime -= 1;
+		fps = fpsCount;
+		fpsCount = 0;
+	}
+	// draw the FPS
+	context.fillStyle = "#f00";
+	context.font = "14px Arial";
+	context.fillText("FPS: " + fps, 5, 20, 100);
+}
+
+
+//-------------------- Don't modify anything below here
+
+
+// This code will set up the framework so that the 'run' function is called 60 times per second.
+// We have a some options to fall back on in case the browser doesn't support our preferred method.
+(function () {
+  var onEachFrame;
+  if (window.requestAnimationFrame) {
+    onEachFrame = function (cb) {
+      var _cb = function () { cb(); window.requestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else if (window.mozRequestAnimationFrame) {
+    onEachFrame = function (cb) {
+      var _cb = function () { cb(); window.mozRequestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else {
+    onEachFrame = function (cb) {
+      setInterval(cb, 1000 / 60);
+    }
+  }
+
+  window.onEachFrame = onEachFrame;
+})();
+
+window.onEachFrame(run);
